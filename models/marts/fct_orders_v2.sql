8bd1f3dc07a3f5db47d47531ae0c14e85bd40740
McKenna Johnson <mckenna.johnson@dbtlabsworkshop.com>
2025-10-02 20:34:25 +0000

    feat: current work

---
+++ models/marts/fct_orders_v2.sql
@@ -0,0 +1,24 @@
+with orders as (
+    select * from {{ ref('int_orders') }}
+),
+
+final as (
+    select 
+        order_id,
+        location_id,
+        customer_id,
+        order_total,
+        tax_paid,
+        net_profit_per_order,
+        ordered_at,
+        customer_name,
+        location_name,
+        opened_date,
+        date_part(month, ordered_at) as ordered_month,
+        date_part(day, ordered_at) as ordered_day, 
+        date_part(year, ordered_at) as ordered_year
+    from orders
+)
+
+select * 
+from final
